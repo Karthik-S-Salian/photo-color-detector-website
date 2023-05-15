8bbde83f5e1163dba4403ebefca9ed016c6a1959
Karthik S Salian <karthikssalian5@gmail.com>
2023-05-15 22:00:21 +0530

    adding comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-{\rtf1}
+Color Picker Project
+This project is a color picker tool that allows users to select colors from an image displayed on a canvas. Users can hover over the image to retrieve the RGB values of the selected color. Additionally, they can toggle a lock to freeze the color selection and copy the RGB value to their clipboard.
+
+Getting Started
+To get started with the project, follow these steps:
+
+Clone the repository to your local machine.
+Open the project in a web browser.
+The canvas will display an image by default (color_street.avif), but you can choose a different image by clicking on the "Choose File" button and selecting an image file.
+Move your mouse over the canvas to select colors from the image. The RGB value of the selected color will be displayed in the "RGB" section.
+If you want to lock the color selection, click on the canvas. This will freeze the selected color, allowing you to copy the RGB value using the "Copy" button.
+Resize the canvas by adjusting the size of the browser window. The canvas will automatically adjust its dimensions to fit the window.
+Dependencies
+The project relies on the following dependencies:
+
+Canvas API: Used to manipulate and draw on the canvas element.
+Clipboard API: Utilized to copy the RGB value to the clipboard.
+DOM API: Used for DOM manipulation and event handling.
+Usage
+Move your mouse over the canvas to hover and select colors from the displayed image.
+The RGB value of the selected color will be shown in the "RGB" section.
+Click on the canvas to lock the color selection. This allows you to copy the RGB value using the "Copy" button.
+Click the "Copy" button to copy the RGB value to your clipboard.
+Resize the canvas by adjusting the size of the browser window.
+Contributing
+Contributions to the project are welcome. If you encounter any issues or have suggestions for improvements, please submit an issue or a pull request.
+
+License
+The project is licensed under the MIT License. Feel free to modify and use the code in your own projects.
--- index.js
+++ index.js
@@ -1,70 +1,98 @@
-
+// Define the radius of the circle and the lock status
 const radius = 8;
-let lock=false;
+let lock = false;
 
+// Define the initial color values
+let color = {
+    red: 0,
+    green: 0,
+    blue: 0
+}
+
+// Get the canvas element and other required DOM elements
 const canvas = document.querySelector('canvas');
 const colorDisplay = document.querySelector("#color-container")
-const colorValueText= document.querySelector("#color-value")
+const colorValueText = document.querySelector("#color-value")
 const dummyImage = new Image();
 dummyImage.src = "images/color_street.avif"
 const context = canvas.getContext('2d');
 const fileChooser = document.getElementById("file-chooser")
+const canvasContainer = document.querySelector("#canvas-container")
 
-
+// Load the dummy image onto the canvas
 //context.drawImage(dummyImage, canvas.width/2-dummyImage.width/2, canvas.height/2-dummyImage.height/2, dummyImage.width, dummyImage.height);
 dummyImage.onload = event => {
-  
-  context.drawImage(dummyImage, 0, 0, dummyImage.width, dummyImage.height, 0, 0, canvas.width, canvas.height);
+    updateCanvas({ offsetX: canvas.width / 2, offsetY: canvas.height / 2 })
 }
 
+// Resize the canvas to fit the window
 function resizeCanvas() {
-  canvas.width = window.innerWidth * .9-48;
-  canvas.height = Math.min(600, window.innerHeight * .8);
+    canvas.width = canvasContainer.offsetWidth;
+    canvas.height = canvasContainer.offsetHeight;
+    updateCanvas({ offsetX: canvas.width / 2, offsetY: canvas.height / 2 });
 }
 
+// Resize the canvas when the window is resized
 onresize = () => {
-  resizeCanvas();
+    resizeCanvas();
 }
 
-addEventListener("DOMContentLoaded", () => {
-  resizeCanvas();
-  
-});
+resizeCanvas();
 
-fileChooser.addEventListener('change', event => {
-  const files = event.target.files;
-  if (files.length !== 0)
-     dummyImage.src = URL.createObjectURL(files[0]);
-     updateCanvas({"x":0,"y":0});
-});
-
-canvas.addEventListener('mousemove', updateCanvas);
-
-
-function updateCanvas(event){
-  const x = event.offsetX;
-  const y = event.offsetY;
-
-  const pixelData = context.getImageData(x, y, 1, 1).data;
-  if(lock)
-    return
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(dummyImage, 0, 0, dummyImage.width, dummyImage.height, 0, 0, canvas.width, canvas.height);
-  context.beginPath();
-  context.arc(x, y, radius, 0, 2 * Math.PI);
-  context.lineWidth = 5;
-  const cssColor=`rgb(${pixelData[0]},${pixelData[1]},${pixelData[2]})`
-  context.strokeStyle = `rgb(${255-pixelData[0]},${255-pixelData[0]},${255-pixelData[0]})`;
-  colorValueText.style.color=`rgb(${255-pixelData[0]},${255-pixelData[0]},${255-pixelData[0]})`;
-  context.stroke();
-  colorValueText.textContent = `RGB: ${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]}`;
-  colorDisplay.style.backgroundColor = cssColor;
-  navigator.clipboard.writeText(cssColor);
-}
-
-canvas.addEventListener("mousedown",function (event){
-  event.preventDefault();
-  lock=!lock;
-  updateCanvas(event);
+// Add event listener to the copy button
+document.getElementById("copy-button").addEventListener("click", () => {
+    navigator.clipboard.writeText(`rgb(${color.red},${color.green},${color.blue})`)
 })
 
+
+// Event listener for file chooser input
+fileChooser.addEventListener('change', event => {
+    const files = event.target.files;
+    if (files.length !== 0)
+        dummyImage.src = URL.createObjectURL(files[0]);
+    updateCanvas({ "x": 0, "y": 0 });
+});
+
+// Event listener for mouse movement on the canvas
+canvas.addEventListener('mousemove', updateCanvas);
+
+// Update the canvas based on the mouse position
+function updateCanvas(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    //if detection is locked don't simply redraw
+    if (lock)
+        return
+
+    // Clear the canvas and draw the dummy image
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(dummyImage, 0, 0, dummyImage.width, dummyImage.height, 0, 0, canvas.width, canvas.height);
+
+    // Get the color data of the pixel at the current position
+    const pixelData = context.getImageData(x, y, 1, 1).data;
+    color.red = pixelData[0]
+    color.green = pixelData[1]
+    color.blue = pixelData[2]
+    const unitColor = 255 - Math.round((color.red + color.green + color.blue) / (255 * 3)) * 255
+
+    // Draw a circle around the current position
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI);
+    context.lineWidth = 5;
+    context.strokeStyle = `rgb(${unitColor},${unitColor},${unitColor})`;
+    context.stroke();
+
+    // Update the color value text and color display
+    colorValueText.style.color = `rgb(${unitColor},${unitColor},${unitColor})`;
+    colorValueText.textContent = `RGB: ${color.red}, ${color.green}, ${color.blue}`;
+    colorDisplay.style.backgroundColor = `rgb(${color.red},${color.green},${color.blue})`;
+
+}
+
+// Event listener for mouse down on the canvas
+canvas.addEventListener("mousedown", function (event) {
+    event.preventDefault();
+    lock = !lock;
+    updateCanvas(event);
+})
